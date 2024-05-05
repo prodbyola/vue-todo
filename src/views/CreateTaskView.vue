153df3ea4aa992ced8727fd3e0a19b4ea09243e5
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-05-05 08:36:39 +0100

    Demostrated Ref API.

--- src/views/CreateTaskView.vue
+++ src/views/CreateTaskView.vue
@@ -1,4 +1,15 @@
 <template>
   <h3>Create Task Page</h3>
-  <p>This page will be used to create new tasks.</p>
+  <p>My state {{ count }}.</p>
+  <button @click="increment">Increment</button>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let count = ref(0)
+const increment = () => {
+  count.value++
+  console.log(count)
+}
+</script>
